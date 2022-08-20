080b3a22522c5a66a46bf33caf412d3145da6534
GHeeJeon <jhjeon_99@naver.com>
2022-08-20 17:49:49 +0900

    Update StringUtilsTest.java

--- StringUtilsTest/src/test/java/org/example/StringUtilsTest.java
+++ StringUtilsTest/src/test/java/org/example/StringUtilsTest.java
@@ -1,5 +1,24 @@
 package org.example;
 
-public class StringUtilsTest {
+import org.apache.commons.lang3.StringUtils;
 
+import static org.junit.Assert.assertTrue;
+
+public class StringUtilsTest {
+    //given
+    //when
+    //then
+
+    @test
+    public void testIsEmpty(){
+        assertTrue("문자열이 비어 있으면 true", StringUtils.isEmpty(""));
+        assertTrue("문자열이 비어 있으면 true", StringUtils.isEmpty(" "));
+        assertTrue("문자열이 비어 있으면 true", StringUtils.isEmpty("GHeeJeon"));
+    }
+
+    public void testIsBlank(){
+        assertTrue("문자열이 비어 있거나 공백만 있으면 있으면 true", StringUtils.isBlank(""));
+        assertTrue("문자열이 비어 있거나 공백만 있으면 있으면 true", StringUtils.isBlank(" "));
+        assertTrue("문자열이 비어 있거나 공백만 있으면 있으면 true", StringUtils.isBlank("GHeeJeon"));
+    }
 }
